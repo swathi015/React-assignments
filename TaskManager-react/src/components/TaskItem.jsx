7a994f338ca3175bed942da9eb9a17656aceb912
swathi015 <thungathurthiswathi06@gmail.com>
2026-03-05 18:08:12 +0530

    Initial commit

--- TaskManager-react/src/components/TaskItem.jsx
+++ TaskManager-react/src/components/TaskItem.jsx
@@ -3,7 +3,7 @@ function TaskItem({ taskObj }) {
     <div
       className={`text-[clamp(16px,10vw,24px)] text-center font-light px-2 py-0.5 rounded ${taskObj.completedStatus && "line-through opacity-80"}`}
     >
-      {taskObj.title} - {taskObj.priority}
+      {taskObj.title} - {taskObj.priority} 
     </div>
   );
 }
