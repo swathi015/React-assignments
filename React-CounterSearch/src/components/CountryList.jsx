703f92ab8e43a5421eaeba50e4220b50dee30f4c
swathi015 <thungathurthiswathi06@gmail.com>
2026-03-05 18:03:29 +0530

    CountryList.jsx

--- React-CounterSearch/src/components/CountryList.jsx
+++ React-CounterSearch/src/components/CountryList.jsx
@@ -1,6 +1,6 @@
 import CountryCard from "./CountryCard";
 
-function CountryList({ countries }) { 
+function CountryList({ countries }) {  
 
   return (
     <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-6">
